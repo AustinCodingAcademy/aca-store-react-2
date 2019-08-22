821a914f3d67230cf29bf76dadcda8ae00452d71
nnijat <nn.aca2019@gmail.com>
2019-08-21 20:11:21 -0500

    Part1 Layout & App is completed

--- public/js/components/ProductDetail.js
+++ public/js/components/ProductDetail.js
@@ -31,28 +31,28 @@ function ProductDetail(props) {
         }
     }
 
-    return (<div className="col-sm-4 col-lg-4 col-md-4">
-        <div className="thumbnail">
-            <img src="http://placehold.it/320x150" alt="" />
-            <div className="caption">
-                <h4 className="pull-right">
-                    {props.product.price}</h4>
-                <h4><a href="#">{props.product.name}</a>
-                </h4>
-                <p>{props.product.description}</p>
+    return (
+        <div className="col-sm-4 col-lg-4 col-md-4">
+            <div className="thumbnail">
+                <img src="http://placehold.it/320x150" alt="" />
+                <div className="caption">
+                    <h4 className="pull-right">
+                        {props.product.price}</h4>
+                    <h4><a href="#">{props.product.name}</a>
+                    </h4>
+                    <p>{props.product.description}</p>
+                </div>
+                <div className="ratings">
+                    <p className="pull-right">
+                        {props.product.reviews} reviews</p>
+                    <p>
+                        {starArr}
+                    </p>
+                </div>
+                <button onClick={() => {
+                    //what do we code here
+                    props.addToCart(props.product);
+                }}>Add To Cart</button>
             </div>
-            <div className="ratings">
-                <p className="pull-right">
-                    {props.product.reviews} reviews</p>
-                <p>
-
-                    {starArr}
-                </p>
-            </div>
-            <button onClick={() => {
-                //what do we code here
-                props.addToCart(props.product);
-            }}>Add To Cart</button>
-        </div>
-    </div>);
+        </div>);
 }
