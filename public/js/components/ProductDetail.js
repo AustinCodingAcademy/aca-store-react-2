f9a3bce612431affaaae7c1f84abd18628725a54
nnijat <nn.aca2019@gmail.com>
2019-08-21 20:03:59 -0500

    Completed Part1 'Building React Components'

--- public/js/components/ProductDetail.js
+++ public/js/components/ProductDetail.js
@@ -9,37 +9,50 @@
     "price": "$95.11"
   }
   */
- function ProductDetail (props){
+function ProductDetail(props) {
 
-    let star = [];
-    for(let i = 0; i < props.product.rating; i ++){
-      star.push(<span key={i} className="glyphicon glyphicon-star"></span>);
+    const stars = props.product.rating;
+    const emptyStars = 5 - stars;
+    let starArr = [];
+
+    // Assume start never more than 5
+    if (stars == 5) {
+        for (let i = 0; i < stars; i++) {
+            starArr.push(<span class="glyphicon glyphicon-star"></span>);
+        }
+    } else {
+        // Add stars to starArr
+        for (let i = 0; i < stars; i++) {
+            starArr.push(<span class="glyphicon glyphicon-star"></span>);
+        }
+        // Then add empty srars to starArr
+        for (let i = 0; i < emptyStars; i++) {
+            starArr.push(<span class="glyphicon glyphicon-star-empty"></span>);
+        }
     }
-    let emptystars = []
-    emptystars.push(<span class="glyphicon glyphicon-star-empty"></span>)
 
-    return ( <div className="col-sm-4 col-lg-4 col-md-4">
-    <div className="thumbnail">
-        <img src="http://placehold.it/320x150" alt=""/>
-        <div className="caption">
-            <h4 className="pull-right">
-            {props.product.price}</h4>
-            <h4><a href="#">{props.product.name}</a>
-            </h4>
-            <p>{props.product.description}</p>
+    return (<div className="col-sm-4 col-lg-4 col-md-4">
+        <div className="thumbnail">
+            <img src="http://placehold.it/320x150" alt="" />
+            <div className="caption">
+                <h4 className="pull-right">
+                    {props.product.price}</h4>
+                <h4><a href="#">{props.product.name}</a>
+                </h4>
+                <p>{props.product.description}</p>
+            </div>
+            <div className="ratings">
+                <p className="pull-right">
+                    {props.product.reviews} reviews</p>
+                <p>
+
+                    {starArr}
+                </p>
+            </div>
+            <button onClick={() => {
+                //what do we code here
+                props.addToCart(props.product);
+            }}>Add To Cart</button>
         </div>
-        <div className="ratings">
-            <p className="pull-right">
-            {props.product.reviews} reviews</p>
-            <p>
-                
-                {star}
-            </p>
-        </div>
-        <button onClick={()=>{
-            //what do we code here
-            props.addToCart(props.product);
-        }}>Add To Cart</button>
-    </div>
-</div>);
+    </div>);
 }
